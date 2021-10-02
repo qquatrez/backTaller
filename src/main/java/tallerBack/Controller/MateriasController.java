23ced5630814822470a8b73e9daa7bed9c487ed3
quatrez <qquatrez@gmail.com>
2021-10-02 18:15:33 -0300

    DeleteMateria PostMateria

--- src/main/java/tallerBack/Controller/MateriasController.java
+++ src/main/java/tallerBack/Controller/MateriasController.java
@@ -3,9 +3,15 @@ package tallerBack.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import tallerBack.Model.MateriasModel;
@@ -23,11 +29,27 @@ public class MateriasController {
 		return materiaService.getAllMaterias();
 	}
 	
+	//falta validar y responder not found cuando no se encuentra codigo
 	@GetMapping("/materias/{codigo}")
 	public MateriasModel getMateriaByCodigo(@PathVariable("codigo")int codigo) {
 		return materiaService.getMateriaByCodigo(codigo);
-		
 	}
+	
+	//delete /falta validar y responder not found cuando no se encuentra codigo
+	@DeleteMapping("/materias/{codigo}")
+	public ResponseEntity<?> deleteMateria(@PathVariable("codigo")int codigo){
+		materiaService.deleteMateria(codigo);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PostMapping("/materias")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void agregarMateria(@RequestBody MateriasModel materia) {
+		materiaService.saveMateria(materia);
+	}
+	
+	
+	
 	
 	
 	
--- src/main/java/tallerBack/Service/MateriasService.java
+++ src/main/java/tallerBack/Service/MateriasService.java
@@ -14,14 +14,25 @@ public class MateriasService {
 	@Autowired
 	private MateriasRepository materiaRepository;
 	
+	//GetAll
 	public List<MateriasModel> getAllMaterias(){
 		return materiaRepository.findAll();
 	}
 	
+	//GetByMateria
 	public MateriasModel getMateriaByCodigo(int codigo) {
 		return materiaRepository.findById(codigo).get();
 	}
 	
+	//Delete
+	public void deleteMateria(int codigo) {
+		materiaRepository.deleteById(codigo);
+	}
+	
+	//Post
+	public void saveMateria(MateriasModel materia) {
+		materiaRepository.save(materia);
+	}
 	
 	
 
