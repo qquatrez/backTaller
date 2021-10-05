bad2494432e424c46bbb5e06b019e0e052fd9182
quatrez <qquatrez@gmail.com>
2021-10-05 19:51:35 -0300

    CrossDomain

--- src/main/java/tallerBack/Controller/MateriasController.java
+++ src/main/java/tallerBack/Controller/MateriasController.java
@@ -6,6 +6,8 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 import tallerBack.Model.MateriasModel;
 import tallerBack.Service.MateriasService;
 
+
+@Validated
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/taller")
 public class MateriasController {
 	
