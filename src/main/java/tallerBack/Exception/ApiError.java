5f9ee22fbf5fc0de26d5dc717a6714803237ab56
quatrez <qquatrez@gmail.com>
2021-10-16 18:59:46 -0300

    ajustes sabado

--- src/main/java/tallerBack/Exception/ApiError.java
+++ src/main/java/tallerBack/Exception/ApiError.java
@@ -1,4 +1,4 @@
-package tallerBack.Model;
+package tallerBack.Exception;
 
 import java.util.List;
 import java.io.Serializable;
--- src/main/java/tallerBack/Exception/ApiErrorHandler.java
+++ src/main/java/tallerBack/Exception/ApiErrorHandler.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import tallerBack.Model.ApiError;
-
 @ControllerAdvice
 public class ApiErrorHandler extends ResponseEntityExceptionHandler {
 	
