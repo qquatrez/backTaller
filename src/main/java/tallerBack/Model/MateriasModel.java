7398daec42da757814f5b555fc3b9fa248ecf395
quatrez <qquatrez@gmail.com>
2021-10-19 20:44:23 -0300

    ajustes menores

--- src/main/java/tallerBack/Model/MateriasModel.java
+++ src/main/java/tallerBack/Model/MateriasModel.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -24,6 +25,7 @@ public class MateriasModel {
 	
 	@NotNull(message = "Cuatrimestre es obligatorio")
 	@Min(1)
+	@Max(10)
 	@Column(name="cuatrimestre")
 	private int cuatrimestre;
 	
